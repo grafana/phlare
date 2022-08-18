856ad0bebf1a729f8c0585b01aef2c411c6c8d44
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-08-18 11:19:15 +0100

    Remove files with incorrect case

--- grafana/flamegraph/src/Constants.ts
+++
@@ -1,7 +0,0 @@
-export const PIXELS_PER_LEVEL = 22;
-export const COLLAPSE_THRESHOLD = 5;
-export const HIDE_THRESHOLD = 0.5;
-export const LABEL_THRESHOLD = 20;
-export const BAR_BORDER_WIDTH = 0.5;
-export const STEP_OFFSET = 4;
-export const NAME_OFFSET = 3;
