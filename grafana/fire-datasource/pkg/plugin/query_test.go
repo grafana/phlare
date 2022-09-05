89d22ae6e9dd059f236a5619d7415f6231c00321
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-09-05 07:32:32 +0100

    Tests

--- grafana/fire-datasource/pkg/plugin/query_test.go
+++ grafana/fire-datasource/pkg/plugin/query_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 	"github.com/stretchr/testify/require"
 
+	commonv1 "github.com/grafana/fire/pkg/gen/common/v1"
 	querierv1 "github.com/grafana/fire/pkg/gen/querier/v1"
 )
 
@@ -38,7 +39,7 @@ func Test_query(t *testing.T) {
 }
 
 // This is where the tests for the datasource backend live.
-func Test_responseToDataFrames(t *testing.T) {
+func Test_profileToDataFrame(t *testing.T) {
 	resp := &connect.Response[querierv1.SelectMergeStacktracesResponse]{
 		Msg: &querierv1.SelectMergeStacktracesResponse{
 			Flamegraph: &querierv1.FlameGraph{
@@ -52,13 +53,38 @@ func Test_responseToDataFrames(t *testing.T) {
 			},
 		},
 	}
-	frame, err := responseToDataFrames(resp)
+	frame, err := profileToDataFrame(resp)
 	require.NoError(t, err)
 	require.Equal(t, []string{"func1", "func2", "func3"}, frame.Meta.Custom.(CustomMeta).Names)
 	require.Equal(t, int64(123), frame.Meta.Custom.(CustomMeta).MaxSelf)
 	require.Equal(t, int64(987), frame.Meta.Custom.(CustomMeta).Total)
 	require.Equal(t, 1, len(frame.Fields))
 	require.Equal(t, data.NewField("levels", nil, []string{"[1,2,3,4]", "[5,6,7,8,9]"}), frame.Fields[0])
+}
+
+func Test_seriesToDataFrame(t *testing.T) {
+	resp := &connect.Response[querierv1.SelectSeriesResponse]{
+		Msg: &querierv1.SelectSeriesResponse{
+			Series: []*querierv1.Series{
+				{Labels: []*commonv1.LabelPair{}, Points: []*querierv1.Point{{T: int64(1000), V: 30}, {T: int64(2000), V: 10}}},
+			},
+		},
+	}
+	frame := seriesToDataFrame(resp, "process_cpu:samples:count:cpu:nanoseconds")
+	require.Equal(t, 2, len(frame.Fields))
+	require.Equal(t, data.NewField("time", nil, []time.Time{time.UnixMilli(1000), time.UnixMilli(2000)}), frame.Fields[0])
+	require.Equal(t, data.NewField("cpu", nil, []float64{30, 10}), frame.Fields[1])
+
+	// with a label pair, the value field should name itself with a label pair name and not the profile type
+	resp = &connect.Response[querierv1.SelectSeriesResponse]{
+		Msg: &querierv1.SelectSeriesResponse{
+			Series: []*querierv1.Series{
+				{Labels: []*commonv1.LabelPair{{Name: "app", Value: "bar"}}, Points: []*querierv1.Point{{T: int64(1000), V: 30}, {T: int64(2000), V: 10}}},
+			},
+		},
+	}
+	frame = seriesToDataFrame(resp, "process_cpu:samples:count:cpu:nanoseconds")
+	require.Equal(t, data.NewField("app", nil, []float64{30, 10}), frame.Fields[1])
 }
 
 type FakeClient struct {
