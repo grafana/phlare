73fba15114e54dd11d493feb8143dc568f67aa1a
Joey Tawadrous <joey.tawadrous@grafana.com>
2022-09-30 10:02:20 +0100

    Reverse both to flamegraph click to reset search based on discussion

--- grafana/flamegraph/src/components/FlameGraphHeader.test.tsx
+++ grafana/flamegraph/src/components/FlameGraphHeader.test.tsx
@@ -32,14 +32,4 @@ describe('FlameGraphHeader', () => {
     screen.getByRole('button', { name: /Reset/i }).click();
     expect(screen.queryByDisplayValue('abc')).not.toBeInTheDocument();
   });
-
-  it('should clear search when flamegraph selected', async () => {
-    render(<FlameGraphHeaderWithProps />);
-
-    // clearing search will also clear the selected item in the top table
-    await userEvent.type(screen.getByPlaceholderText('Search..'), 'abc');
-    expect(screen.getByDisplayValue('abc')).toBeInTheDocument();
-    screen.getByText(/Flame Graph/).click();
-    expect(screen.queryByDisplayValue('abc')).not.toBeInTheDocument();
-  });
 });
