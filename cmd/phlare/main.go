167122b9a99674ee7515117976d0b483898c236e
hi-rustin <rustin.liu@gmail.com>
2023-02-04 20:47:32 +0800

    Parse main flags first

--- cmd/phlare/main.go
+++ cmd/phlare/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"sort"
 
@@ -22,13 +23,13 @@ func (mf *mainFlags) registerFlags(fs *flag.FlagSet) {
 
 func main() {
 	var (
-		mf     mainFlags
 		config phlare.Config
 	)
 
-	mf.registerFlags(flag.CommandLine)
+	// Register main flags and parse them first.
+	mf, args := parseMainFlags(os.Args[1:])
 
-	if err := cfg.DynamicUnmarshal(&config, os.Args[1:], flag.CommandLine); err != nil {
+	if err := cfg.DynamicUnmarshal(&config, args, flag.CommandLine); err != nil {
 		fmt.Fprintf(os.Stderr, "failed parsing config: %v\n", err)
 		os.Exit(1)
 	}
@@ -63,4 +64,27 @@ func main() {
 		fmt.Fprintf(os.Stderr, "failed running phlare: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func parseMainFlags(args []string) (mainFlags, []string) {
+	var mf mainFlags
+	leftArgs := make([]string, 0, len(args))
+
+	// Continue parsing flags if there is an error.
+	fs := flag.NewFlagSet("", flag.ContinueOnError)
+	fs.SetOutput(io.Discard)
+
+	// Register main flags and parse them first.
+	mf.registerFlags(fs)
+	// Try to find all main flags in the arguments.
+	// As Parsing stops on the first error, e.g. unknown flag, we simply
+	// try remaining parameters until we find config flag, or there are no params left.
+	// Put all other flags into leftArgs.
+	for i := range args {
+		if err := fs.Parse(args[i:]); err != nil {
+			leftArgs = append(leftArgs, args[i])
+		}
+	}
+
+	return mf, leftArgs
 }
