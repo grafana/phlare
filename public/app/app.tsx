ae6a5a5498acaba8f703bccd3115b10f0e24aea6
Eduardo Aleixo <eduardo@pyroscope.io>
2023-03-31 11:47:58 +0100

    add a rudimentary 'dev' command for quick iterative dev

--- public/app/app.tsx
+++ public/app/app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('reactRoot'));
+const root = ReactDOM.createRoot(document.getElementById('reactRoot'));
+root.render(<div>Hello world</div>);
