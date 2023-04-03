20ef942d2a03c355d65c5b9041f1b953fdf7c6ff
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-03 14:35:02 +0100

    remove 'hello world' message to make it mergeable

--- public/app/app.tsx
+++ public/app/app.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('reactRoot'));
-root.render(<div>Hello world</div>);
+root.render(<div></div>);
