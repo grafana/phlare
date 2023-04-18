02fadede8ddac35740be65558e791b6322fc686a
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-18 16:43:28 +0100

    backfill between series items

--- pkg/querier/timeline.go
+++ pkg/querier/timeline.go
@@ -12,25 +12,45 @@ import (
 // * Non-zero
 // * startMs is earlier than the first series value
 // * endMs is after the last series value
-// TODO: sample
 func NewTimeline(series *v1.Series, startMs int64, endMs int64) *flamebearer.FlamebearerTimelineV1 {
 	points := series.GetPoints()
 	durationDeltaInSec := int64(10)
 
 	firstAvailableData := points[0]
-	howManyToBackfillStart := (firstAvailableData.Timestamp - startMs) / 1000 / durationDeltaInSec
+	lastAvailableData := points[len(points)-1]
 
 	// TODO: what if it returns < 0
 	// Backfill with 0s for data that's not available
-	backFillStart := make([]uint64, int(howManyToBackfillStart))
-
-	lastAvailableData := points[len(points)-1]
-	howManyToBackfillEnd := (endMs - lastAvailableData.Timestamp) / 1000 / durationDeltaInSec
-	backFillEnd := make([]uint64, int(howManyToBackfillEnd))
+	backFillStart := backfill(startMs, firstAvailableData.Timestamp, durationDeltaInSec)
+	backFillEnd := backfill(lastAvailableData.Timestamp, endMs, durationDeltaInSec)
 
 	samples := make([]uint64, len(points))
-	for i, p := range points {
-		samples[i] = uint64(p.Value)
+
+	i := 0
+	prev := points[0]
+	for _, p := range points {
+		backfillNum := needsToBackfill(prev.Timestamp, p.Timestamp, durationDeltaInSec)
+
+		if backfillNum > 0 {
+			// backfill + newValue
+			bf := append(backfill(prev.Timestamp, p.Timestamp, durationDeltaInSec), uint64(p.Value))
+
+			// break the slice
+			first := samples[:i]
+			second := samples[i:]
+
+			// add new backfilled items
+			first = append(first, bf...)
+
+			// concatenate the three slices to form the new slice
+			samples = append(first, second...)
+			prev = p
+			i = i + int(backfillNum)
+		} else {
+			samples[i] = uint64(p.Value)
+			prev = p
+			i = i + 1
+		}
 	}
 
 	samples = append(backFillStart, samples...)
@@ -45,4 +65,22 @@ func NewTimeline(series *v1.Series, startMs int64, endMs int64) *flamebearer.Fla
 	}
 
 	return timeline
+}
+
+func needsToBackfill(startMs int64, endMs int64, stepSec int64) int64 {
+	startSec := startMs / 1000
+	endSec := endMs / 1000
+	size := ((endSec - startSec) - stepSec) / stepSec
+	return size
+}
+
+func backfill(startMs int64, endMs int64, stepSec int64) []uint64 {
+	startSec := startMs / 1000
+	endSec := endMs / 1000
+
+	size := ((endSec - startSec) - stepSec) / stepSec
+	if size <= 0 {
+		size = 0
+	}
+	return make([]uint64, size)
 }
--- pkg/querier/timeline_test.go
+++ pkg/querier/timeline_test.go
@@ -9,13 +9,32 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNoBackfillData(t *testing.T) {
+	d := time.Date(2023, time.April, 18, 1, 2, 3, 4, time.UTC)
+
+	points := &typesv1.Series{
+		Points: []*typesv1.Point{
+			{Timestamp: d.UnixMilli(), Value: 99},
+		},
+	}
+
+	timeline := querier.NewTimeline(points, d.UnixMilli(), d.UnixMilli())
+
+	assert.Equal(t, d.UnixMilli()/1000, timeline.StartTime)
+	assert.Equal(t, []uint64{
+		99,
+	}, timeline.Samples)
+}
+
 func TestBackfillData(t *testing.T) {
 	d := time.Date(2023, time.April, 18, 1, 2, 3, 4, time.UTC)
 	startTime := d.Add(-1 * time.Minute).UnixMilli()
 	endTime := d.Add(1 * time.Minute).UnixMilli()
 
 	points := &typesv1.Series{
-		Points: []*typesv1.Point{{Timestamp: d.UnixMilli(), Value: 99}},
+		Points: []*typesv1.Point{
+			{Timestamp: d.UnixMilli(), Value: 99},
+		},
 	}
 
 	timeline := querier.NewTimeline(points, startTime, endTime)
@@ -23,8 +42,33 @@ func TestBackfillData(t *testing.T) {
 	assert.Equal(t, startTime/1000, timeline.StartTime)
 	assert.Equal(t, []uint64{
 		// 1 point for each 10 seconds
-		0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
 		99,
-		0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0,
+	}, timeline.Samples)
+}
+
+func TestBackfillData2(t *testing.T) {
+	d := time.Date(2023, time.April, 18, 1, 2, 3, 4, time.UTC)
+	startTime := d.Add(-1 * time.Minute).UnixMilli()
+	endTime := d.Add(1 * time.Minute).UnixMilli()
+
+	points := &typesv1.Series{
+		Points: []*typesv1.Point{
+			{Timestamp: d.UnixMilli(), Value: 99},
+			{Timestamp: d.Add(20 * time.Second).UnixMilli(), Value: 98},
+		},
+	}
+
+	timeline := querier.NewTimeline(points, startTime, endTime)
+
+	assert.Equal(t, startTime/1000, timeline.StartTime)
+	assert.Equal(t, []uint64{
+		// 1 point for each 10 seconds
+		0, 0, 0, 0, 0,
+		99,
+		0,
+		98,
+		0, 0, 0, 0,
 	}, timeline.Samples)
 }
