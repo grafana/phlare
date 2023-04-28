f3e5a17f04285d8f9f42d6163206ae4707358a9e
Eduardo Aleixo <eduardo@pyroscope.io>
2023-04-28 14:29:33 +0100

    fix tests

--- public/app/overrides/util/baseurl.spec.ts
+++ public/app/overrides/util/baseurl.spec.ts
@@ -1,5 +1,4 @@
-import baseurl from './baseurl';
-import { detectBaseurl } from './baseurl';
+import baseurlForAPI, { baseurl } from './baseurl';
 
 function mockSelector(href: string) {
   const base = document.createElement('base');
@@ -7,10 +6,10 @@ function mockSelector(href: string) {
   return base;
 }
 
-describe('baseurl', () => {
+describe('baseurlForAPI', () => {
   describe('no baseurl has been detected', () => {
     it('returns undefined', () => {
-      const got = baseurl();
+      const got = baseurlForAPI();
       expect(got).toBe(undefined);
     });
   });
@@ -22,56 +21,41 @@ describe('baseurl', () => {
           .spyOn(document, 'querySelector')
           .mockImplementationOnce(() => mockSelector('/pyroscope/ui'));
 
-        const got = baseurl();
+        const got = baseurlForAPI();
         expect(got).toBe('/pyroscope');
-      });
-    });
-
-    describe('it does not contain /ui', () => {
-      // Until we move the /ui page to the root, this is absolutely required
-      it('throws an error', () => {
-        jest
-          .spyOn(document, 'querySelector')
-          .mockImplementationOnce(() => mockSelector('/pyroscope/'));
-
-        expect(() => baseurl()).toThrowError();
       });
     });
   });
 });
 
-describe('detectBaseurl', () => {
+describe('baseurl', () => {
   describe('no baseURL meta tag set', () => {
     it('returns undefined', () => {
-      const got = detectBaseurl();
+      const got = baseurl();
       expect(got).toBe(undefined);
     });
   });
 
   describe('base tag is set', () => {
     describe('a relative path is passed', () => {
-      // This test ends up testing some functionality for 'base' tag
-      // which is left for documentating purposes
-      it('prepends with the host', () => {
+      it('uses as is', () => {
         jest
           .spyOn(document, 'querySelector')
           .mockImplementationOnce(() => mockSelector('/pyroscope'));
 
-        const got = detectBaseurl();
-        expect(got).toBe('http://localhost/pyroscope');
+        const got = baseurl();
+        expect(got).toBe('/pyroscope');
       });
     });
 
     describe('a full url is passed', () => {
-      it('uses as is', () => {
+      it('strips the origin', () => {
         jest
           .spyOn(document, 'querySelector')
-          .mockImplementationOnce(() =>
-            mockSelector('http://localhost:9999/pyroscope')
-          );
+          .mockImplementationOnce(() => mockSelector('/pyroscope'));
 
-        const got = detectBaseurl();
-        expect(got).toBe('http://localhost:9999/pyroscope');
+        const got = baseurl();
+        expect(got).toBe('/pyroscope');
       });
     });
   });
--- public/app/overrides/util/baseurl.ts
+++ public/app/overrides/util/baseurl.ts
@@ -1,30 +1,22 @@
 /**
- basename returns the "href" value of the <base> tag if available
- otherwise it assumes it's running under root, and location.origin is used
- it also expects it will only be run in the browser
+ basename checks the "href" value of the <base> tag if available
+ and returns the pathname part
+ otherwise it return undefined
  */
 export function baseurl() {
-  const href = detectBaseurl();
-  if (!href) {
-    return undefined;
-  }
-
-  const url = new URL(href, window.location.href);
-  return url.pathname;
-}
-
-export function baseurlForAPI() {
-  // When serving production, api path is one level above /ui
-  return baseurl()?.replace('/ui', '');
-}
-
-function detectBaseurl(): string | undefined {
   const base = document.querySelector('base') as HTMLBaseElement;
   if (!base) {
     return undefined;
   }
 
-  return base.href;
+  const url = new URL(base.href, window.location.href);
+  return url.pathname;
+}
+
+export function baseurlForAPI() {
+  // When serving production, api path is one level above /ui
+  // TODO(eh-am): remove when pages are moved to root
+  return baseurl()?.replace('/ui', '');
 }
 
 export default baseurlForAPI;
