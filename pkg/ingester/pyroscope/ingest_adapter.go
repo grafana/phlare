14bcbf1d972e05e945702741cf111c5b0e650e67
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-05-25 01:28:28 +0800

    Update ingest_adapter.go

--- pkg/ingester/pyroscope/ingest_adapter.go
+++ pkg/ingester/pyroscope/ingest_adapter.go
@@ -99,7 +99,7 @@ func (p *pyroscopeIngesterAdapter) Put(ctx context.Context, pi *storage.PutInput
 		Name:  labels.MetricName,
 		Value: metric,
 	}, &typesv1.LabelPair{
-		Name:  "__service_name__",
+		Name:  "service_name",
 		Value: app,
 	}, &typesv1.LabelPair{
 		Name:  phlaremodel.LabelNameDelta,
