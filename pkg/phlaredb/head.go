20a815ed0c7c2f31229ba7843a79ae180340a12d
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2023-06-22 19:27:41 +0200

    Fix symbols upload

--- pkg/phlaredb/head.go
+++ pkg/phlaredb/head.go
@@ -1007,7 +1007,7 @@ func (h *Head) SymDBFiles() ([]block.File, error) {
 		if f.IsDir() {
 			continue
 		}
-		result[idx].RelPath = filepath.Join(h.headPath, f.Name())
+		result[idx].RelPath = filepath.Join(symdb.DefaultDirName, f.Name())
 		info, err := f.Info()
 		if err != nil {
 			return nil, err
