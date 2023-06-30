d54626bfd91297b88d262d9326aae37448bde47e
Tolya Korniltsev <korniltsev.anatoly@gmail.com>
2023-06-30 10:38:50 +0700

    debug metrics

--- ebpf/symtab/metrics.go
+++ ebpf/symtab/metrics.go
@@ -15,27 +15,27 @@ func NewMetrics(reg prometheus.Registerer) *Metrics {
 	m := &Metrics{
 		ElfErrors: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_elf_errors_total",
-			Help: "",
+			Help: "Total number of errors while trying to open an elf file",
 		}, []string{"error"}),
 		ProcErrors: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_proc_errors_total",
-			Help: "",
+			Help: "Total number of errors while trying refreshing /proc/pid/maps",
 		}, []string{"error"}),
 		KnownSymbols: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_known_symbols_total",
-			Help: "",
+			Help: "Total number of successfully resolved symbols",
 		}, []string{"service_name"}),
 		UnknownSymbols: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_unknown_symbols_total",
-			Help: "",
+			Help: "Total number of unresolved symbols for a module",
 		}, []string{"service_name"}),
 		UnknownModules: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_unknown_modules_total",
-			Help: "",
+			Help: "Total number of unknown modules - could not find an entry in /proc/pid/maps for a RIP",
 		}, []string{"service_name"}),
 		UnknownStacks: prometheus.NewCounterVec(prometheus.CounterOpts{
 			Name: "pyroscope_symtab_unknown_stacks_total",
-			Help: "",
+			Help: "Total number of stacks with unknowns > knowns",
 		}, []string{"service_name"}),
 	}
 
