c046c3508284ecd35380f9800aeee82a8a541cfc
Anton Kolesnikov <anton.e.kolesnikov@gmail.com>
2023-07-17 21:06:28 +0800

    Add lookup table test

--- pkg/phlaredb/compact_test.go
+++ pkg/phlaredb/compact_test.go
@@ -3,12 +3,11 @@ package phlaredb
 import (
 	"context"
 	"net/http"
+	_ "net/http/pprof"
 	"sort"
 	"sync"
 	"testing"
 	"time"
-
-	_ "net/http/pprof"
 
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/storage"
@@ -132,6 +131,15 @@ func testCompact(t *testing.T, metas []*block.Meta, bkt phlareobj.Bucket, dst st
 		"numSamples", new.Stats.NumSamples)
 }
 
+type blockReaderMock struct {
+	BlockReader
+	idxr IndexReader
+}
+
+func (m *blockReaderMock) Index() IndexReader {
+	return m.idxr
+}
+
 func TestProfileRowIterator(t *testing.T) {
 	filePath := t.TempDir() + "/index.tsdb"
 	idxw, err := index.NewWriter(context.Background(), filePath)
@@ -158,7 +166,7 @@ func TestProfileRowIterator(t *testing.T) {
 			{SeriesIndex: 1, TimeNanos: 2},
 			{SeriesIndex: 2, TimeNanos: 3},
 		},
-	), idxr)
+	), &blockReaderMock{idxr: idxr})
 	require.NoError(t, err)
 
 	assert.True(t, it.Next())
@@ -190,4 +198,97 @@ func TestProfileRowIterator(t *testing.T) {
 func addSeries(t *testing.T, idxw *index.Writer, idx int, labels phlaremodel.Labels) {
 	t.Helper()
 	require.NoError(t, idxw.AddSeries(storage.SeriesRef(idx), labels, model.Fingerprint(labels.Hash()), index.ChunkMeta{SeriesIndex: uint32(idx)}))
+}
+
+func Test_lookupTable(t *testing.T) {
+	// Given the source data set.
+	// Copy arbitrary subsets of those items to dst.
+	var dst []string
+	src := []string{
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+	}
+
+	type testCase struct {
+		description string
+		input       []uint32
+		expected    []string
+	}
+
+	testCases := []testCase{
+		{
+			description: "empty table",
+			input:       []uint32{5, 0, 3, 1, 2, 2, 4},
+			expected:    []string{"five", "zero", "three", "one", "two", "two", "four"},
+		},
+		{
+			description: "no new values",
+			input:       []uint32{2, 1, 2, 3},
+			expected:    []string{"two", "one", "two", "three"},
+		},
+		{
+			description: "new value mixed",
+			input:       []uint32{2, 1, 6, 2, 3},
+			expected:    []string{"two", "one", "six", "two", "three"},
+		},
+	}
+
+	// Try to lookup values in src lazily.
+	// Table size must be greater or equal
+	// to the source data set.
+	l := newLookupTable[string](10)
+
+	populate := func(t *testing.T, x []uint32) {
+		for i, v := range x {
+			x[i] = l.tryLookup(v)
+		}
+		// Resolve unknown yet values.
+		// Mind the order and deduplication.
+		p := -1
+		for it := l.iter(); it.Err() == nil && it.Next(); {
+			m := int(it.At())
+			if m <= p {
+				t.Fatal("iterator order invalid")
+			}
+			p = m
+			it.setValue(src[m])
+		}
+	}
+
+	resolveAppend := func() {
+		// Populate dst with the newly resolved values.
+		// Note that order in dst does not have to match src.
+		for _, n := range l.unresolved {
+			l.storeResolved(n.rid, uint32(len(dst)))
+			dst = append(dst, n.val)
+		}
+	}
+
+	resolve := func(x []uint32) []string {
+		// Lookup resolved values.
+		var resolved []string
+		for _, v := range x {
+			resolved = append(resolved, dst[l.lookupResolved(v)])
+		}
+		return resolved
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			l.reset()
+			populate(t, tc.input)
+			resolveAppend()
+			assert.Equal(t, tc.expected, resolve(tc.input))
+		})
+	}
+
+	assert.Len(t, dst, 7)
+	assert.NotContains(t, dst, "seven")
 }
